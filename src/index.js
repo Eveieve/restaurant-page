1261de49ecfedcb11b3ce6cbb90a59d8bbe62e87
Mike <nhj25252@gmail.com>
2023-03-24 13:19:59 +0900

    Display About Tab

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import { Home, Menu } from './module.js';
+import { Home, Menu, About } from './module.js';
 
 const home = Home();
 home.content();
+
+// const about = About();
+// about.section1();
