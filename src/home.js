b4c6469264cee663f4b7e49baa4ad613eab2fe3b
Mike <nhj25252@gmail.com>
2023-03-27 16:06:35 +0900

    create renderHomeContent function

--- src/home.js
+++ src/home.js
@@ -6,6 +6,12 @@ const root = document.querySelector('#content');
 const rootContent = document.createElement('div');
 document.body.appendChild(rootContent);
 
+const renderHomeContent = () => {
+  const p = document.createElement('p');
+  p.textContent = 'This is a home content';
+  rootContent.appendChild(p);
+};
+
 const renderNavbar = () => {
   const nav = document.createElement('nav');
   const menu = document.createElement('li');
@@ -21,6 +27,9 @@ const renderNavbar = () => {
   nav.appendChild(contact);
   root.appendChild(nav);
 
+  console.log(root);
+  console.log(rootContent);
+
   menu.addEventListener('click', () => {
     while (rootContent.firstChild) {
       rootContent.removeChild(rootContent.lastChild);
@@ -32,10 +41,11 @@ const renderNavbar = () => {
     while (rootContent.firstChild) {
       rootContent.removeChild(rootContent.lastChild);
     }
-    renderAbout();
+    // renderAbout();
   });
 };
 
 export default function renderHome() {
   renderNavbar();
+  renderHomeContent();
 }
